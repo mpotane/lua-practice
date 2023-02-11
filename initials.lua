e4de043ffc099ee051db0818b69ba80cb2ec26c7
mpotane <1000100ten@google.com>
2023-02-11 22:30:32 +0800

    First commit

---
+++ initials.lua
@@ -0,0 +1,11 @@
+--[[
+This is a comment
+]]--
+
+print("BBBB     SSS ")
+print("B    B  S   S")
+print("B    B  S    ")
+print("BBBB     SSS ")
+print("B    B      S")
+print("B    B  S   S")
+print("BBBB     SSS ")
